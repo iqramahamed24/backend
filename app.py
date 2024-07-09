fb2ca4888eefc27631c3c54a1c56b442d5590658
CharlesKibet <charlesbiegon973@gmail.com>
2024-07-09 17:30:27 +0300

    Fetching of users from our database

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
-#This are the views or what the end user will see
+# This are the views or what the end user will see
 from flask import Flask
 from flask_migrate import Migrate
 
-from models import db,User
+from models import db, User
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def home():
-  return '<h1>Manage your funds more easily</h1>'
+    return '<h1>Manage your funds more easily</h1>'
+
 
 @app.route('/users')
 def users():
-   users = User.query.all()
-   print(users)
-   return []
-   
-if __name__ == '__main__':
-  app.run()
 
+    users = User.query.all()
+    users_list = []
+    for user in users:
+        users_list.append(user.to_dict())
+    print(users_list)
+    return []
+
+
+if __name__ == '__main__':
+    app.run()
