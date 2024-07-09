7c2b2a205e18b18bfbb49a4f19029b807d367d5b
CharlesKibet <charlesbiegon973@gmail.com>
2024-07-09 16:43:34 +0300

    App.py set up and the seed.py set up

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 #This are the views or what the end user will see
+from flask import Flask
+from flask_migrate import Migrate
+
+from models import db,User
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] 
+
+migrate = Migrate(app,db)
+
+@app.route('/')
+def home():
+  return '<h1>Manage your funds more easily</h1>'
+
+@app.route('/users')
+def users():
+   users = User.query.all()
+   print(users)
