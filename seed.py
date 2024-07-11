714686ff65168933445da760ecc93c77993d6389
CharlesKibet <charlesbiegon973@gmail.com>
2024-07-11 09:39:34 +0300

    Seeding data to the income and budget

--- seed.py
+++ seed.py
@@ -1,22 +1,22 @@
 # For our dummy data when testing out our application
 from datetime import datetime
 from app import app
-from models import db, User,Income
+from models import db, User, Income, Budget
 with app.app_context():
     print("Inputting data")
     User.query.delete()
     Income.query.delete()
-    # Budget.query.delete()
+    Budget.query.delete()
     # Expense.query.delete()
 
     # To append our new users to this empty array
     new_user = []
 
-    charles = User(user_name="Charles Kibet", email="charles@gmail.com",password="charles"
-                  )
+    charles = User(user_name="Charles Kibet", email="charles@gmail.com", password="charles"
+                   )
     new_user.append(charles)
 
-    guido = User(user_name="Guido Python", email="guido@gmail.com",password="charles"
+    guido = User(user_name="Guido Python", email="guido@gmail.com", password="charles"
                  )
     new_user.append(guido)
 
@@ -25,8 +25,20 @@ with app.app_context():
     print("Users added successfully")
 
     new_income = []
-    income = Income(amount=4000, user=charles, date=datetime.now())
-    income = Income(amount=4000, user=guido, date=datetime.now())
+    income1 = Income(user=charles, amount=4000, date=datetime.now())
+    income2 = Income(user=guido, amount=4000, date=datetime.now())
+    new_income.append(income1)
+    new_income.append(income2)
     db.session.add_all(new_income)
     db.session.commit()
     print("Income added successfully")
+    
+    
+    new_budget = []
+    budget1 = Budget(amount=4000,description="Entertainment")
+    budget2 = Budget(amount=4000,description="Entertainment")
+    new_budget.append(budget1)
+    new_budget.append(budget1)
+    db.session.add_all(new_budget)
+    db.session.commit()
+    print("Budgets added successfully")
