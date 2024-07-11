28f5206c1c42cb5cb1d927c1e7ae028b24d3aff2
Anthony Ngonde <anto.ngonde@gmail.com>
2024-07-11 11:47:43 +0300

    register view added

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # This are the views or what the end user will see
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_migrate import Migrate
+from datetime import datetime
+from functools import wraps
 
-from models import db, User
+from models import db, User, Expense, Budget
 
 app = Flask(__name__)
 
@@ -18,6 +20,23 @@ def home():
     return '<h1>Manage your funds more easily</h1>'
 
 
+# def token_required(f):
+#     @wraps(f)
+#     def decorated(*args, **kwargs):
+#         token = None
+#         if 'Authorization' in request.headers:
+#             token = request.headers['Authorization'].split(" ")[1]
+#         if not token:
+#             return jsonify({'message': 'Token is missing!'}), 401
+#         try:
+#             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+#             current_user = User.query.filter_by(id=data['user_id']).first()
+#         except:
+#             return jsonify({'message': 'Token is invalid!'}), 401
+#         return f(current_user, *args, **kwargs)
+#     return decorated
+
+
 @app.route('/users')
 def users():
 
@@ -27,6 +46,148 @@ def users():
         users_list.append(user.to_dict())
     print(users_list)
     return []
+
+
+
+# Register User
+@app.route('/register', methods = ['POST'])
+def register():
+    data = request.get_json()
+    new_user = User(user_name = data['user_name'], email = data['email'], created_at = datetime.now())
+
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.to_dict())
+
+
+
+# Login User
+app.route('/login', methods = ['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email = data['email']).first()
+    if user and user.verify_password(data['password']):
+        token = user.generate_auth_token()
+        return jsonify({'token': token}), 200
+    return jsonify({'message': 'Invalid credentials'}), 401
+
+
+
+# # Add Expense
+# @app.route('/expenses', methods = ['POST'])
+# # @token_required
+# def add_expense(current_user):
+#     data = request.get_json()
+#     new_expense = Expense(amount = data['amount'], category = data['category'], description = data['description'], created_at = datetime.now(), user_id = current_user.id)
+
+#     db.session.add(new_expense)
+#     db.session.commit()
+#     return jsonify(new_expense.to_dict()), 201
+
+
+
+
+# # Get Expenses
+# @app.route('/expenses', methods = ['GET'])
+# # @token_required
+# def get_expenses(current_user):
+#     expenses = Expense.query.filter_by(user_id = current_user.id).all()
+
+#     return jsonify([expense.to_dict() for expense in expenses]), 200
+
+
+
+# # Update Expense
+# @app.route('/expenses/int:id>', methods = ['PATCH'])
+# # @token_required
+# def update_expense(current_user, id):
+#     expense = Expense.query.get(id)
+#     if not expense or expense.user_id != current_user.id:
+#         return jsonify({'message': 'Expense not found'}), 400
+    
+#     data = request.get_json()
+#     expense.amount = data['amount']
+#     expense.category = data['category']
+#     expense.description = data['description']
+#     db.session.commit()
+#     return jsonify(expense.to_dict()), 200
+
+
+
+# # Delete Expense
+# @app.route('/expenses/<int:id>', methods = ['DELETE'])
+# # @token_required
+# def delete_expense(current_user, id):
+#     expense = Expense.query.get(id)
+#     if not expense or expense.user_id != current_user.id:
+#         return jsonify({'message': 'Expense not found'}), 404
+#     db.session.delete(expense)
+#     db.session.commit()
+#     return '', 204
+
+
+
+
+# # Add Budget
+# @app.route('/budgets', methods = '[POST]')
+# # @token_required
+# def add_budget(current_user):
+#     data = request.get_json()
+#     new_budget = Budget(
+#         amount = data['amount'],
+#         balance = data['balance'],
+#         created_at = datetime.now()
+#     )
+#     db.session.add(new_budget)
+#     db.session.commit()
+#     return jsonify(new_budget.to_dict()), 201
+
+
+
+
+# # Get Budgets
+# @app.route('/budgets', methods = ['GET'])
+# # @token_required
+# def get_budgets(current_user):
+#     budgets = Budget.query.filter_by(user_id = current_user.id).all()
+
+#     return jsonify([budget.to_dict() for budget in budgets]), 200
+
+
+
+# # Update Budget
+# @app.route('/budgets/<int:id>', methods = ['PATCH'])
+# # @token_required
+# def update_budget(current_user, id):
+#     budget = Budget.query.get(id)
+#     if not budget or budget.user_id != current_user.id:
+#         return jsonify({'message', 'Budget not found'}), 404
+    
+#     data = request.get_json()
+#     budget.amount = data['amount']
+#     budget.balance = data['balance']
+#     db.session.commit()
+#     return jsonify(budget.to_dict()), 200
+
+
+
+
+# # Delete Budget
+# @app.route('/budgets/int:id', methods = ['DELETE'])
+# # @token_required
+# def delete_budget(current_user, id):
+#     budget = Budget.query.get(id)
+#     if not budget or budget.user_id != current_user.id:
+#         return jsonify({'message': "Budget not found"}), 404
+    
+#     db.session.delete(budget)
+#     db.session.commit()
+#     return '', 204
+
+
+
+
+
 
 
 if __name__ == '__main__':
