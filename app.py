45c93d2db81585332379f032c946e03305a291ee
CharlesKibet <charlesbiegon973@gmail.com>
2024-07-15 13:33:42 +0300

    Cors set up

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 import os
 from flask import Flask
 from flask_migrate import Migrate
-from flask_restful import Resource, Api
+from flask_restful import  Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-
+from flask_cors import CORS
 
 from models import db
 from resources.user import Register, Login
@@ -19,6 +19,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_ECHO'] = True
 
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
+
+CORS(app)
 
 migrate = Migrate(app, db, render_as_batch=True)
 
