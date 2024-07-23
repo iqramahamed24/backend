01d21a40a859f54652fa091d7cec3e0ad0c7b8bb
CharlesKibet <charlesbiegon973@gmail.com>
2024-07-23 11:19:50 +0300

    Link to render hosting added

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ from resources.income import IncomeResource
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_ECHO'] = True
 
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
